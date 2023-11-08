2c443ded7e519d5f1956e3333cdc61d4ff5f581c
SungMinCho-Kor <rhxoeh0011@gmail.com>
2023-11-08 20:12:10 +0900

    Feat : 프로젝트 생성

---
+++ iOS/Village/Village/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Village
+//
+//  Created by 조성민 on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
