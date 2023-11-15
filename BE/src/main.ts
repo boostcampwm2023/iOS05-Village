8b2bf81f1d0c49c2f95b6b71f25f26e50e61943e
이광훈 <leegh963@naver.com>
2023-11-15 21:59:44 +0900

    [BE] Feat : winstonLogger 기능 적용
     - winstonLogger

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './utils/swagger';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
+import { dailyOption, winstonOptions } from './utils/winston';
+import * as winstonDaily from 'winston-daily-rotate-file';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        winstonOptions,
+        new winstonDaily(dailyOption('error')),
+        new winstonDaily(dailyOption('info')),
+      ],
+    }),
+  });
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   setupSwagger(app);
   await app.listen(3000);
 }
---
+++ BE/src/utils/winston.ts
@@ -0,0 +1,36 @@
+import * as winston from 'winston';
+import * as winstonDaily from 'winston-daily-rotate-file';
+import {
+  utilities as nestWinstonModuleUtilities,
+  utilities,
+  WinstonModule,
+} from 'nest-winston';
+
+export const winstonOptions = new winston.transports.Console({
+  level: process.env.NODE_ENV === 'prod' ? 'info' : 'silly',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.colorize({ all: true }),
+    nestWinstonModuleUtilities.format.nestLike('MyApp', {
+      prettyPrint: true,
+    }),
+  ),
+});
+
+export const dailyOption = (level: string) => {
+  return {
+    level,
+    datePattern: 'YYYY-MM-DD',
+    dirname: `./logs/${level}`,
+    filename: `%DATE%.${level}.log`,
+    maxFiles: 30,
+    zippedArchive: true,
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      utilities.format.nestLike(process.env.NODE_ENV, {
+        colors: false,
+        prettyPrint: true,
+      }),
+    ),
+  };
+};
