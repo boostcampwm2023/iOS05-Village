71322888b9fe0f516ece34da106de3524a8349bd
koomin1227 <koomin1227@naver.com>
2023-11-15 15:42:39 +0900

    [BE] Feat: 환경변수 설정
    Resolves: #34
    
    Co-authored-by: namewhat99 <leegh963@naver.com>

--- BE/src/app.module.ts
+++ BE/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'process';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
