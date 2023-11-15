450f60af8f62593783059a932bab9010c3bc327b
koomin1227 <koomin1227@naver.com>
2023-11-16 00:06:34 +0900

    [BE] Feat: mysql 설정 provider 생성

---
+++ BE/src/config/mysql.config.ts
@@ -0,0 +1,21 @@
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class MysqlConfigProvider implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'mysql',
+      host: this.configService.get('DB_HOST'),
+      port: this.configService.get<number>('DB_PORT'),
+      username: this.configService.get('DB_USERNAME'),
+      password: this.configService.get('DB_PASSWORD'),
+      database: this.configService.get('DB_DATABASE'),
+      entities: [],
+      synchronize: true,
+    };
+  }
+}
