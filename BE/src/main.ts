07f301c737a1587cbb6da928d43e055e23599c59
koomin1227 <koomin1227@naver.com>
2023-11-15 15:10:22 +0900

    [BE] Feat: swagger 설정
    Resolves: #32
    
    Co-authored-by: namewhat99 <leegh963@naver.com>

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupSwagger } from './utils/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
