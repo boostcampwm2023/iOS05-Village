a879533651501de56fcc9c4e8290feb81961ce31
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-17 22:57:17 +0900

    [iOS] Feat : DetailPostViewController 생성

---
+++ iOS/Village/Village/Presentation/DetailPost/DetailPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailPostViewController.swift
+//  Village
+//
+//  Created by 박동재 on 2023/11/17.
+//
+
+import UIKit
+
+class DetailPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
