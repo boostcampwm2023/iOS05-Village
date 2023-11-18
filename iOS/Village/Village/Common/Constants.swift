ebffcbe6349a33607493456057df158d9fdd8d19
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-18 13:46:19 +0900

    [iOS] Feat : ImageSystemName 추가
    - ImageSystemName 추가

--- iOS/Village/Village/Common/Constants.swift
+++ iOS/Village/Village/Common/Constants.swift
@@ -12,4 +12,10 @@ enum ImageSystemName: String {
     case photo
     case magnifyingGlass = "magnifyingglass"
     case chevronRight = "chevron.right"
+    case home = "house"
+    case fillHome = "house.fill"
+    case message = "message"
+    case fillMessage = "message.fill"
+    case myPage = "person"
+    case fillMyPage = "person.fill"
 }
