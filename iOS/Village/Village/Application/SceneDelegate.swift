83634d14d0f695accdb25a24701543ae0a36f731
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-18 13:47:04 +0900

    [iOS] Feat : TapBarController 구현
    - TapBarController 구현

--- iOS/Village/Village/Application/SceneDelegate.swift
+++ iOS/Village/Village/Application/SceneDelegate.swift
@@ -15,8 +15,32 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: HomeViewController())
-        window?.rootViewController = navigationController
+        
+        let tapBarController = UITabBarController()
+        let homeViewController = UINavigationController(rootViewController: HomeViewController())
+        let chatViewController = UINavigationController(rootViewController: HomeViewController())
+        let myPageViewController = UINavigationController(rootViewController: HomeViewController())
+        
+        tapBarController.setViewControllers(
+            [homeViewController, chatViewController, myPageViewController],
+            animated: true
+        )
+        
+        if let items = tapBarController.tabBar.items {
+            items[0].selectedImage = UIImage(systemName: ImageSystemName.fillHome.rawValue)
+            items[0].image = UIImage(systemName: ImageSystemName.home.rawValue)
+            items[0].title = "홈"
+            
+            items[1].selectedImage = UIImage(systemName: ImageSystemName.fillMessage.rawValue)
+            items[1].image = UIImage(systemName: ImageSystemName.message.rawValue)
+            items[1].title = "채팅"
+            
+            items[2].selectedImage = UIImage(systemName: ImageSystemName.fillMyPage.rawValue)
+            items[2].image = UIImage(systemName: ImageSystemName.myPage.rawValue)
+            items[2].title = "내 정보"
+        }
+        
+        window?.rootViewController = tapBarController
         window?.makeKeyAndVisible()
     }
 
