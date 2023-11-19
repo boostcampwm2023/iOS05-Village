6112c8fa2fba198cd3afdb42e3287608a03f5e08
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-19 13:40:10 +0900

    [iOS] Chore : 피드백 반영

--- iOS/Village/Village/Presentation/Chat/ChatViewController.swift
+++ iOS/Village/Village/Presentation/Chat/ChatViewController.swift
@@ -12,7 +12,6 @@ class ChatViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .blue
     }
 
 }
--- iOS/Village/Village/Presentation/MyPage/MyPageViewController.swift
+++ iOS/Village/Village/Presentation/MyPage/MyPageViewController.swift
@@ -12,7 +12,6 @@ class MyPageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .green
     }
     
 }
