d22c0a81ac1a8722d267c816811ded3f7b416794
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-20 23:35:25 +0900

    [iOS] Chore : 피드백 반영
    - 피드백 반영

--- iOS/Village/Village/Presentation/ChatRoom/ChatRoomViewController.swift
+++ iOS/Village/Village/Presentation/ChatRoom/ChatRoomViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ChatRoomViewController: UIViewController {
+final class ChatRoomViewController: UIViewController {
     
     let chatStackView: UIStackView = {
         let stackView = UIStackView()
