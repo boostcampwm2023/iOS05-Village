5bfb1cd9d84178b1ad5de7d0574934d33c00a3ed
koomin1227 <koomin1227@naver.com>
2023-11-21 01:01:58 +0900

    [BE] Feat : S3 multer factory 구현

---
+++ BE/src/post/multer-options.factory.ts
@@ -0,0 +1,29 @@
+import { S3Client } from '@aws-sdk/client-s3';
+import { Request } from 'express';
+import { MulterOptions } from '@nestjs/platform-express/multer/interfaces/multer-options.interface';
+import * as multerS3 from 'multer-s3';
+import { ConfigService } from '@nestjs/config';
+
+export const multerOptionsFactory = (
+  configService: ConfigService,
+): MulterOptions => {
+  return {
+    storage: multerS3({
+      s3: new S3Client({
+        endpoint: configService.get('S3_ENDPOINT'),
+        region: configService.get('S3_REGION'),
+        credentials: {
+          accessKeyId: configService.get('S3_ACCESS_KEY'),
+          secretAccessKey: configService.get('S3_SECRET_KEY'),
+        },
+      }),
+      bucket: configService.get('S3_BUCKET'),
+      acl: 'public-read',
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      key: (req: Request, file, callback) => {
+        const fileName = file.originalname;
+        callback(null, fileName);
+      },
+    }),
+  };
+};
