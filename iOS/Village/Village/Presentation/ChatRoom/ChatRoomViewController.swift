957718aff4c8c95f7f594621d147b9de9e649d89
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-20 22:33:26 +0900

    [iOS] Feat : 채팅방 거래 물건 뷰 구현
    - 채팅방 거래 물건 뷰 구현

--- iOS/Village/Village/Presentation/ChatRoom/ChatRoomViewController.swift
+++ iOS/Village/Village/Presentation/ChatRoom/ChatRoomViewController.swift
@@ -9,6 +9,50 @@ import UIKit
 
 class ChatRoomViewController: UIViewController {
     
+    let postView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: ImageSystemName.photo.rawValue)
+        imageView.contentMode = .scaleAspectFit
+        imageView.backgroundColor = .primary100
+        imageView.layer.cornerRadius = 16
+        
+        return imageView
+    }()
+    
+    let postTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "닌텐도 스위치"
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        
+        return label
+    }()
+    
+    let postPriceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "10000원"
+        label.font = .systemFont(ofSize: 14, weight: .bold)
+        
+        return label
+    }()
+    
+    let postAccessoryView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(systemName: ImageSystemName.chevronRight.rawValue)
+        
+        return imageView
+    }()
+    
     let chatStackView: UIStackView = {
         let stackView = UIStackView()
         stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -54,6 +98,12 @@ class ChatRoomViewController: UIViewController {
     
     private func setUI() {
         
+        postView.addSubview(postImageView)
+        postView.addSubview(postTitleLabel)
+        postView.addSubview(postPriceLabel)
+        postView.addSubview(postAccessoryView)
+        view.addSubview(postView)
+        
         chatTextField.delegate = self
         
         chatStackView.addArrangedSubview(chatMoreButton)
@@ -65,6 +115,36 @@ class ChatRoomViewController: UIViewController {
     }
     
     private func configureConstraints() {
+        
+        NSLayoutConstraint.activate([
+            postView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            postView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            postView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            postView.heightAnchor.constraint(equalToConstant: 100)
+        ])
+        
+        NSLayoutConstraint.activate([
+            postImageView.topAnchor.constraint(equalTo: postView.topAnchor, constant: 10),
+            postImageView.leadingAnchor.constraint(equalTo: postView.leadingAnchor, constant: 10),
+            postImageView.widthAnchor.constraint(equalToConstant: 80),
+            postImageView.heightAnchor.constraint(equalToConstant: 80)
+        ])
+        
+        NSLayoutConstraint.activate([
+            postTitleLabel.topAnchor.constraint(equalTo: postView.topAnchor, constant: 25),
+            postTitleLabel.leadingAnchor.constraint(equalTo: postImageView.trailingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            postPriceLabel.topAnchor.constraint(equalTo: postTitleLabel.bottomAnchor, constant: 10),
+            postPriceLabel.leadingAnchor.constraint(equalTo: postImageView.trailingAnchor, constant: 20)
+        ])
+        
+        NSLayoutConstraint.activate([
+            postAccessoryView.topAnchor.constraint(equalTo: postView.topAnchor, constant: 39),
+            postAccessoryView.trailingAnchor.constraint(equalTo: postView.trailingAnchor, constant: -16)
+        ])
+        
         NSLayoutConstraint.activate([
             chatStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             chatStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
