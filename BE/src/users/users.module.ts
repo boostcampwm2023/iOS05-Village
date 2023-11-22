9e86246e42ff494aab8b7e14125add176910fd61
koomin1227 <koomin1227@naver.com>
2023-11-22 19:37:18 +0900

    [BE] Feat : uses.module에 typeORM 설정

--- BE/src/users/users.module.ts
+++ BE/src/users/users.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostEntity } from '../entities/post.entity';
+import { UserEntity } from '../entities/user.entity';
+import { PostImageEntity } from '../entities/postImage.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
   providers: [UsersService],
 })
