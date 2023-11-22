f8c3c1b76b129674bee04e08b319a3bb769644cf
koomin1227 <koomin1227@naver.com>
2023-11-22 20:39:09 +0900

    [BE] Feat : user 세부정보를 조회하는 컨트롤러 구현

--- BE/src/users/users.controller.ts
+++ BE/src/users/users.controller.ts
@@ -9,6 +9,7 @@ import {
   UseInterceptors,
   UploadedFiles,
   ValidationPipe,
+  HttpException,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './createUser.dto';
@@ -18,6 +19,16 @@ import { MultiPartBody } from 'src/utils/multiPartBody.decorator';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get(':id')
+  async usersDetails(@Param('id') userId) {
+    const user = await this.usersService.findUserById(userId);
+    if (user === null) {
+      throw new HttpException('유저가 존재하지않습니다.', 404);
+    } else {
+      return user;
+    }
+  }
 
   @Post()
   @UseInterceptors(FileInterceptor('profileImage'))
