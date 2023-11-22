79a53791f85fb5194613af367fe585539e71c638
이광훈 <leegh963@naver.com>
2023-11-22 15:14:55 +0900

    [BE] Chore : Git cherry-pick test

---
+++ BE/src/users/users.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFiles,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './createUser.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { MultiPartBody } from 'src/utils/multiPartBody.decorator';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('profileImage'))
+  async usersCreate(
+    @UploadedFiles() files: Express.Multer.File,
+    @MultiPartBody(
+      'profile',
+      new ValidationPipe({ validateCustomDecorators: true }),
+    )
+    createUserDto: CreateUserDto,
+  ) {
+    const user = await this.usersService.createUser(createUserDto);
+    return user;
+  }
+
+  @Delete(':id')
+  usersRemove(@Param('id') id: string) {
+    return this.usersService.removeUser(+id);
+  }
+}
