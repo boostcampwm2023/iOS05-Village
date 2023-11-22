302c693cb316cdb03a2ace51386bdf6cd17372ab
koomin1227 <koomin1227@naver.com>
2023-11-22 20:38:35 +0900

    [BE] Feat : id로 유저를 찾는 service 로직 구현

--- BE/src/users/users.service.ts
+++ BE/src/users/users.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './createUser.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+  async findUserById(userId: string) {
+    const user: UserEntity = await this.userRepository.findOne({
+      where: { user_hash: userId, status: true },
+    });
+    if (user) {
+      return { nickname: user.nickname, profile_img: user.profile_img };
+    } else {
+      return null;
+    }
+  }
   createUser(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
