f7129b94b35f4c9abbf86f84dac0d742cf0f8930
jsangyun <jsangyun1039@naver.com>
2023-11-22 23:15:41 +0900

    [iOS] Feat : RequestDetailViewController 클래스 추가

---
+++ iOS/Village/Village/Presentation/PostDetail/RequestDetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RequestDetailViewController.swift
+//  Village
+//
+//  Created by 정상윤 on 11/22/23.
+//
+
+import UIKit
+
+final class RequestDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
