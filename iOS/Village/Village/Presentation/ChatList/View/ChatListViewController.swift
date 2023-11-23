26dc80dc940a4c8f166eb198a494dc6a52d99328
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-23 13:48:27 +0900

    [iOS] Feat : ChatListViewController 생성
    - ChatListViewController 생성

---
+++ iOS/Village/Village/Presentation/ChatList/View/ChatListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatListViewController.swift
+//  Village
+//
+//  Created by 박동재 on 2023/11/23.
+//
+
+import UIKit
+
+class ChatListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
