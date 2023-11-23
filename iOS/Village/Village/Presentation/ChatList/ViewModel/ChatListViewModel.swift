efc7472374362b21aea7c915bc6dd8cef25ce3c6
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-24 00:02:12 +0900

    [iOS] Feat : ChatListResponseDTO 생생
    - ChatListResponseDTO 생생

---
+++ iOS/Village/Village/Presentation/ChatList/ViewModel/ChatListViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  ChatListViewModel.swift
+//  Village
+//
+//  Created by 박동재 on 2023/11/23.
+//
+
+import Foundation
+
+struct ChatListResponseDTO: Hashable, Codable {
+    let user: String
+    let userProfile: String?
+    let recentTime: String
+    let recentChat: String
+    let postImage: String
+    
+    enum CodingKeys: String, CodingKey {
+        case user
+        case userProfile = "user_profile"
+        case recentTime = "recent_time"
+        case recentChat = "recent_chat"
+        case postImage = "post_image"
+    }
+}
+
+final class ChatListViewModel {
+    
+}
