2c10262c12f5642c561117903f8461b48870244d
koomin1227 <koomin1227@naver.com>
2023-11-23 18:24:11 +0900

    [BE] Feat : 게시물 차단 요청에 대한 컨트롤러 작성

--- BE/src/posts-block/posts-block.controller.ts
+++ BE/src/posts-block/posts-block.controller.ts
@@ -1,7 +1,12 @@
 import { Controller, Param, Post } from '@nestjs/common';
+import { PostsBlockService } from './posts-block.service';
 
 @Controller('posts/block')
 export class PostsBlockController {
+  constructor(private readonly postsBlockService: PostsBlockService) {}
   @Post(':id')
-  postsBlockCreate(@Param('id') PostId) {}
+  async postsBlockCreate(@Param('id') postId: number) {
+    const blockerId = 'qwe';
+    await this.postsBlockService.createPostsBlock(blockerId, postId);
+  }
 }
