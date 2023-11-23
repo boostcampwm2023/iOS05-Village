197d914f8fd7e5fd9e35147714e131516b6ac264
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-23 14:06:40 +0900

    [iOS] Feat : ChatListCollectionViewCell 생성
    - ChatListCollectionViewCell 생성

--- iOS/Village/Village/Presentation/ChatList/View/ChatListCollectionViewCell.swift
+++ iOS/Village/Village/Presentation/ChatList/View/ChatListCollectionViewCell.swift
@@ -9,4 +9,12 @@ import UIKit
 
 class ChatListCollectionViewCell: UICollectionViewCell {
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("Should not be called")
+    }
+
 }
