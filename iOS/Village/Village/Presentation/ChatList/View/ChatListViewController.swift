9f751ed889c1b98f841ff6ca36d8625fa7caea25
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-23 14:46:32 +0900

    [iOS] CollectionView 틀 구현
    - CollectionView 틀 구현

--- iOS/Village/Village/Presentation/ChatList/View/ChatListViewController.swift
+++ iOS/Village/Village/Presentation/ChatList/View/ChatListViewController.swift
@@ -8,18 +8,30 @@
 import UIKit
 
 class ChatListViewController: UIViewController {
+    
+    typealias ChatListDataSource = UICollectionViewDiffableDataSource<Section, String>
+    
+    private var dataSource: ChatListDataSource!
     private let reuseIdentifier = ChatListCollectionViewCell.identifier
+    private var collectionView: UICollectionView!
+    
+    enum Section {
+        case chat
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUI()
+        generateData()
     }
     
     private func setUI() {
         view.backgroundColor = .systemBackground
         
         setNavigationUI()
+        configureCollectionView()
+        configureDataSource()
     }
 
     private func setNavigationUI() {
@@ -28,4 +40,60 @@ class ChatListViewController: UIViewController {
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: titleLabel)
         self.navigationItem.backButtonDisplayMode = .minimal
     }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.delegate = self
+        view.addSubview(collectionView)
+    }
+    
+    private func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .absolute(100.0)
+        )
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 1)
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.contentInsets = .init(top: 10.0, leading: 0.0, bottom: 4.0, trailing: 0.0)
+        section.interGroupSpacing = 8.0
+        
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    private func configureDataSource() {
+        collectionView.register(HomeCollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        dataSource = ChatListDataSource(collectionView: collectionView) { (collectionView, indexPath, chatList) ->
+            UICollectionViewCell? in
+            guard let cell = collectionView.dequeueReusableCell(
+                withReuseIdentifier: self.reuseIdentifier,
+                for: indexPath
+            ) as? HomeCollectionViewCell else {
+                return UICollectionViewCell()
+            }
+            
+            return cell
+        }
+    }
+    
+    private func generateData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, String>()
+        snapshot.appendSections([.chat])
+        
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+}
+
+extension ChatListViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
 }
