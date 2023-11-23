b610d14c43530bf7549905ae4572c731071d0735
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-23 13:58:02 +0900

    [iOS] Feat : ChatListCollectionViewCell 생성
    ChatListCollectionViewCell 생성

---
+++ iOS/Village/Village/Presentation/ChatList/View/ChatListCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChatListCollectionViewCell.swift
+//  Village
+//
+//  Created by 박동재 on 2023/11/23.
+//
+
+import UIKit
+
+class ChatListCollectionViewCell: UICollectionViewCell {
+    
+}
