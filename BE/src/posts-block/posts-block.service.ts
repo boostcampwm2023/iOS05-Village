acc9453c449bbf2f36c189b47126a660675e1614
koomin1227 <koomin1227@naver.com>
2023-11-23 18:23:38 +0900

    [BE] Feat : 차단 게시물을 등록하는 로직 작성

--- BE/src/posts-block/posts-block.service.ts
+++ BE/src/posts-block/posts-block.service.ts
@@ -1,4 +1,37 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostEntity } from '../entities/post.entity';
+import { Repository } from 'typeorm';
+import { BlockPostEntity } from '../entities/blockPost.entity';
 
 @Injectable()
-export class PostsBlockService {}
+export class PostsBlockService {
+  constructor(
+    @InjectRepository(PostEntity)
+    private postRepository: Repository<PostEntity>,
+    @InjectRepository(BlockPostEntity)
+    private blockPostRepository: Repository<BlockPostEntity>,
+  ) {}
+  async createPostsBlock(blockerId: string, postId: number) {
+    const blockedPost = await this.postRepository.findOne({
+      where: { id: postId },
+    });
+    if (!blockedPost) {
+      throw new HttpException('없는 게시물입니다.', 404);
+    }
+    const blockPostEntity = new BlockPostEntity();
+    const isExist = await this.blockPostRepository.exist({
+      where: {
+        blocker: blockerId,
+        blocked_post: postId,
+      },
+    });
+    if (isExist) {
+      throw new HttpException('이미 차단 되었습니다.', 400);
+    }
+    blockPostEntity.blocked_post = postId;
+    blockPostEntity.blocker = blockerId;
+    blockPostEntity.status = true;
+    await this.blockPostRepository.save(blockPostEntity);
+  }
+}
