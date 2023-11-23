549177a302bcd3fef45459bbd4e36e64550205af
koomin1227 <koomin1227@naver.com>
2023-11-23 18:22:15 +0900

    [BE] Feat : postBlockService에 repository 주입

--- BE/src/posts-block/posts-block.module.ts
+++ BE/src/posts-block/posts-block.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { PostsBlockController } from './posts-block.controller';
 import { PostsBlockService } from './posts-block.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlockPostEntity } from '../entities/blockPost.entity';
+import { PostEntity } from '../entities/post.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BlockPostEntity, PostEntity])],
   controllers: [PostsBlockController],
-  providers: [PostsBlockService]
+  providers: [PostsBlockService],
 })
 export class PostsBlockModule {}
