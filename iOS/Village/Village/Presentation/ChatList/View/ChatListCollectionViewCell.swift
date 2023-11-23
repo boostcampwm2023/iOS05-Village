6dd9e8be50c53a28d8c2fe09dfd089feaf70f82b
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-23 14:18:50 +0900

    [iOS] Feat : Cell View 생성
    - Cell View 생성

--- iOS/Village/Village/Presentation/ChatList/View/ChatListCollectionViewCell.swift
+++ iOS/Village/Village/Presentation/ChatList/View/ChatListCollectionViewCell.swift
@@ -9,6 +9,55 @@ import UIKit
 
 class ChatListCollectionViewCell: UICollectionViewCell {
     
+    private let chatView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 24
+        
+        return imageView
+    }()
+    
+    private let nicknameLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16, weight: .bold)
+        
+        return label
+    }()
+    
+    private let recentTimeLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 14, weight: .regular)
+        
+        return label
+    }()
+    
+    private let recentChatLabel: UILabel = {
+       let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 16, weight: .regular)
+        
+        return label
+    }()
+    
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        imageView.layer.cornerRadius = 16
+        
+        return imageView
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
