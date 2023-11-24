08b550219f59d4eaa71eb0cc5f8211b9f9c6738f
jsangyun <jsangyun1039@naver.com>
2023-11-24 10:18:41 +0900

    [iOS] Fix : PostListItem 데이터 타입 수정, 대여/요청 게시글 상세 이동 코드 수정

--- iOS/Village/Village/Presentation/Home/Model/PostListItem.swift
+++ iOS/Village/Village/Presentation/Home/Model/PostListItem.swift
@@ -11,16 +11,16 @@ struct PostListItem: Hashable {
     
     let title: String
     let price: Int?
-    let postID: String
-    let userID: String
+    let postID: Int
+    let userID: Int
     let isRequest: Int
     let imageURL: String?
     
-    init(dto: PostResponseDTO) {
+    init(dto: PostListResponseDTO) {
         self.title = dto.title
         self.price = dto.price
-        self.postID = "\(dto.postID)"
-        self.userID = "\(dto.userID)"
+        self.postID = dto.postID
+        self.userID = dto.userID
         self.isRequest = dto.isRequest
         self.imageURL = dto.images.first
     }
