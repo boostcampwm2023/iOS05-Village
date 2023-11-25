efb09787236582f45e1c6b6dc58d24d7453cf3a8
이광훈 <leegh963@naver.com>
2023-11-25 17:28:09 +0900

    [BE] Feat : http 로거 설정

--- BE/src/main.ts
+++ BE/src/main.ts
@@ -5,6 +5,7 @@ import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
 import { dailyOption, winstonOptions } from './config/winston.config';
 import * as winstonDaily from 'winston-daily-rotate-file';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpLoggerInterceptor } from './utils/httpLogger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -16,7 +17,7 @@ async function bootstrap() {
       ],
     }),
   });
-
+  app.useGlobalInterceptors(new HttpLoggerInterceptor());
   app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(
     new ValidationPipe({
@@ -24,6 +25,7 @@ async function bootstrap() {
     }),
   );
   setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
