6f6f9125d32e2f2bc29a643e934e395ad9d95772
koomin1227 <koomin1227@naver.com>
2023-11-26 19:45:59 +0900

    [BE] Feat : test 코드 수정

--- BE/src/app.controller.spec.ts
+++ BE/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Village API Server"', () => {
+      expect(appController.home()).toBe('Village API Server');
     });
   });
 });
