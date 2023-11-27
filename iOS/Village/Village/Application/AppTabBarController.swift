01156b82a300b3eb06ffef6c76245bfa00b27fab
jsangyun <jsangyun1039@naver.com>
2023-11-27 17:46:31 +0900

    [iOS] Feat : AppTabBarController 클래스 추가

---
+++ iOS/Village/Village/Application/AppTabBarController.swift
@@ -0,0 +1,48 @@
+//
+//  AppTabBarController.swift
+//  Village
+//
+//  Created by 정상윤 on 11/27/23.
+//
+
+import UIKit
+
+final class AppTabBarController: UITabBarController {
+    
+    private let homeViewController = UINavigationController(rootViewController: HomeViewController())
+    private let chatListViewController = UINavigationController(rootViewController: ChatListViewController())
+    private let myPageViewController = UINavigationController(rootViewController: MyPageViewController())
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tabBar.tintColor = .primary500
+        tabBar.unselectedItemTintColor = .primary500
+        setViewControllers()
+        configureTabBarItems()
+    }
+    
+    private func setViewControllers() {
+        setViewControllers(
+            [homeViewController, chatListViewController, myPageViewController],
+            animated: true
+        )
+    }
+    
+    private func configureTabBarItems() {
+        if let items = tabBar.items {
+            items[0].selectedImage = UIImage(systemName: ImageSystemName.houseFill.rawValue)
+            items[0].image = UIImage(systemName: ImageSystemName.house.rawValue)
+            items[0].title = "홈"
+            
+            items[1].selectedImage = UIImage(systemName: ImageSystemName.messageFill.rawValue)
+            items[1].image = UIImage(systemName: ImageSystemName.message.rawValue)
+            items[1].title = "채팅"
+            
+            items[2].selectedImage = UIImage(systemName: ImageSystemName.personFill.rawValue)
+            items[2].image = UIImage(systemName: ImageSystemName.person.rawValue)
+            items[2].title = "내 정보"
+        }
+    }
+    
+}
