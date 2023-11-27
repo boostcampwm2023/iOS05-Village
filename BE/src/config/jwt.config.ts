7c8790d1348066a47fe74a4961b8e0f9099801a4
koomin1227 <koomin1227@naver.com>
2023-11-27 17:37:16 +0900

    [BE] Feat : JWT config 하여 모듈에 추가

---
+++ BE/src/config/jwt.config.ts
@@ -0,0 +1,16 @@
+import { ConfigService } from '@nestjs/config';
+import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
+
+export class JwtConfig implements JwtOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createJwtOptions(): Promise<JwtModuleOptions> | JwtModuleOptions {
+    const jwtModuleOptions: JwtModuleOptions = {
+      secret: this.configService.get('JWT_SECRET'),
+      signOptions: {
+        expiresIn: this.configService.get('JWT_EXPIRES_IN'),
+      },
+    };
+    return jwtModuleOptions;
+  }
+}
--- BE/src/login/login.module.ts
+++ BE/src/login/login.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtConfig } from '../config/jwt.config';
 
 @Module({
+  imports: [JwtModule.registerAsync({ useClass: JwtConfig })],
   controllers: [LoginController],
   providers: [LoginService],
 })
