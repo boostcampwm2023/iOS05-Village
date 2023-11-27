8980f0ddbeddeb6f5147f2ce3b374e48a66dd74d
동재 <59719500+jea0716@users.noreply.github.com>
2023-11-27 15:42:13 +0900

    [iOS] Feat : ChatRoomCollectionViewCell 및 identifier 익스텐션 생성
    - ChatRoomCollectionViewCell 및 identifier 익스텐션 생성

---
+++ iOS/Village/Village/Presentation/ChatRoom/View/ChatRoomCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ChatRoomCollectionViewCell.swift
+//  Village
+//
+//  Created by 박동재 on 2023/11/27.
+//
+
+import UIKit
+
+class ChatRoomCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ iOS/Village/Village/Presentation/Utils/Extensions/UIView+Identifier.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Identifier.swift
+//  Village
+//
+//  Created by 박동재 on 2023/11/27.
+//
+
+import UIKit
+
+extension UIView {
+
+    static var identifier: String {
+        return String(describing: self)
+    }
+
+}
