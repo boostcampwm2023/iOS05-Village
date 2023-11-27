545592c70f713f3fdfd1b1f10f51a511302e0936
koomin1227 <koomin1227@naver.com>
2023-11-27 21:58:28 +0900

    [BE] Feat : jwt 모듈 등록

--- BE/src/config/jwt.config.ts
+++ BE/src/config/jwt.config.ts
@@ -1,10 +1,12 @@
 import { ConfigService } from '@nestjs/config';
 import { JwtModuleOptions, JwtOptionsFactory } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common';
 
+@Injectable()
 export class JwtConfig implements JwtOptionsFactory {
   constructor(private configService: ConfigService) {}
 
-  createJwtOptions(): Promise<JwtModuleOptions> | JwtModuleOptions {
+  createJwtOptions(): JwtModuleOptions {
     const jwtModuleOptions: JwtModuleOptions = {
       secret: this.configService.get('JWT_SECRET'),
       signOptions: {
--- BE/src/login/login.module.ts
+++ BE/src/login/login.module.ts
@@ -3,9 +3,14 @@ import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtConfig } from '../config/jwt.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
 
 @Module({
-  imports: [JwtModule.registerAsync({ useClass: JwtConfig })],
+  imports: [
+    JwtModule.registerAsync({ useClass: JwtConfig }),
+    TypeOrmModule.forFeature([UserEntity]),
+  ],
   controllers: [LoginController],
   providers: [LoginService],
 })
