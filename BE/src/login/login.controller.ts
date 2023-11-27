2fbe844214d4fd601b2f96d0eb5c87018272b843
koomin1227 <koomin1227@naver.com>
2023-11-27 22:01:06 +0900

    [BE] Feat : 소셜 로그인 기능 구현

--- BE/src/login/login.controller.ts
+++ BE/src/login/login.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, HttpException, Post } from '@nestjs/common';
-import { JwtTokens, LoginService, SocialProperties } from './login.service';
+import { LoginService, SocialProperties } from './login.service';
 import { AppleLoginDto } from './appleLoginDto';
 
 @Controller('login')
@@ -8,19 +8,11 @@ export class LoginController {
 
   @Post('appleOAuth') // 임시
   async signInWithApple(@Body() body: AppleLoginDto) {
-    // const socialProperties: SocialProperties =
-    //   await this.loginService.appleOAuth(body);
-    const socialProperties = {
-      OAuthDomain: 'apple',
-      socialId: 'user123@example.com',
-    };
+    const socialProperties: SocialProperties =
+      await this.loginService.appleOAuth(body);
     if (!socialProperties) {
       throw new HttpException('토큰이 유효하지 않음', 401);
     }
-    const tokens: JwtTokens = await this.loginService.login(socialProperties);
-    if (!tokens) {
-      throw new HttpException('가입된 유저가 없음', 404);
-    }
-    return tokens;
+    return await this.loginService.login(socialProperties);
   }
 }
