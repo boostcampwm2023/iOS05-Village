fadba18b668b5f52e0c28c2b10452d30ab830c78
koomin1227 <koomin1227@naver.com>
2023-11-28 14:32:16 +0900

    [BE] Feat : refresh token 기능 구현

--- BE/src/login/login.controller.ts
+++ BE/src/login/login.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, HttpException, Post } from '@nestjs/common';
 import { LoginService, SocialProperties } from './login.service';
-import { AppleLoginDto } from './appleLoginDto';
+import { AppleLoginDto } from './dto/appleLogin.dto';
 
 @Controller('login')
 export class LoginController {
@@ -14,5 +14,20 @@ export class LoginController {
       throw new HttpException('토큰이 유효하지 않음', 401);
     }
     return await this.loginService.login(socialProperties);
+  }
+
+  @Post('refresh')
+  async refreshToken(@Body('refresh_token') refreshToken) {
+    try {
+      const payload = this.loginService.validateToken(refreshToken, 'refresh');
+      return await this.loginService.refreshToken(payload);
+    } catch (e) {
+      throw new HttpException('refresh token이 유효하지 않음', 403);
+    }
+  }
+
+  @Post('admin')
+  loginAdmin(@Body('user') user) {
+    return this.loginService.loginAdmin(user);
   }
 }
