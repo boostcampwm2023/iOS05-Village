598e14799b24ee8c13b183bc0fb2b1e820a8efbe
SungMinCho-Kor <rhxoeh0011@gmail.com>
2023-11-29 18:00:33 +0900

    [iOS] Fix : gitignore 적용 시키기 위해 git cache 삭제

--- iOS/Village/Village/Application/AppDelegate.swift
+++ iOS/Village/Village/Application/AppDelegate.swift
@@ -22,6 +22,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
     
+    
+}
+
+extension AppDelegate {
+    
     func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
         let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
         let token = tokenParts.joined()
@@ -32,15 +37,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print("Failed to register: \(error)")
     }
     
-}
-
-extension AppDelegate {
-    
     func registerForPushNotifications() {
+        UNUserNotificationCenter.current().delegate = self
         UNUserNotificationCenter.current()
             .requestAuthorization(options: [.alert, .sound, .badge]) { [weak self] granted, _ in
-                print("Permission granted: \(granted)")
-                
                 guard granted else { return }
                 self?.getNotificationSettings()
             }
@@ -48,12 +48,30 @@ extension AppDelegate {
     
     func getNotificationSettings() {
         UNUserNotificationCenter.current().getNotificationSettings { settings in
-            print("Notification settings: \(settings)")
             guard settings.authorizationStatus == .authorized else { return }
             DispatchQueue.main.async {
                 UIApplication.shared.registerForRemoteNotifications()
             }
         }
     }
+    
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    // 앱이 foreground상태 일 때, 알림이 온 경우 어떻게 표현할 것인지 처리
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
+    }
+
+    // push를 탭한 경우 처리
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+
+        let content = response.notification.request.content.userInfo
+        print("contents = \(content)")
+    }
+
     
 }
---
+++ iOS/Village/Village/Presentation/Utils/Extensions/NotificationName+.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationName+.swift
+//  Village
+//
+//  Created by 조성민 on 11/29/23.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let openChatRoom = Notification.Name("OpenChatRoom")
+}
