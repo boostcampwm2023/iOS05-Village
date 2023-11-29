a6b09d4cecfaaaeba3d35bd2d0787517561acfd9
jsangyun <jsangyun1039@naver.com>
2023-11-30 00:50:51 +0900

    [iOS] Feat : LoginViewModel TokenManager를 통한 토큰 키체인 저장 및 불러오기 구현

--- iOS/Village/Village/Presentation/Login/ViewModel/LoginViewModel.swift
+++ iOS/Village/Village/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -17,22 +17,31 @@ final class LoginViewModel {
         Publishers.Zip(input.identityToken, input.authorizationCode)
             .sink(receiveValue: { [weak self] (token, code) in
                 guard let tokenString = String(data: token, encoding: .utf8),
-                      let codeString = String(data: code, encoding: .utf8) else { return }
+                      let codeString = String(data: code, encoding: .utf8),
+                      let email = tokenString.decode()["email"] as? String else { return }
                 
-                self?.handleLoginRequest(dto: AppleOAuthDTO(identityToken: tokenString, authorizationCode: codeString))
+                let dto = AppleOAuthDTO(identityToken: tokenString, authorizationCode: codeString)
+                self?.handleLoginRequest(email: email, dto: dto)
             })
             .store(in: &cancellableBag)
         
         return Output(authenticationToken: authenticationToken.eraseToAnyPublisher())
     }
     
-    private func handleLoginRequest(dto: AppleOAuthDTO) {
-        // TODO: 키체인에 저장된 토큰이 있는지 확인 후 서버 요청하도록 구현
+    func autoLogin() -> Bool {
+        guard let token = JWTManager.shared.get() else { return false }
+        
+        return true
+    }
+    
+    private func handleLoginRequest(email: String, dto: AppleOAuthDTO) {
         let endpoint = APIEndPoints.loginAppleOAuth(with: dto)
         Task {
             do {
-                guard let responseToken = try await APIProvider.shared.request(with: endpoint) else { return }
-                authenticationToken.send(responseToken)
+                guard let token = try await APIProvider.shared.request(with: endpoint) else { return }
+                
+                JWTManager.shared.save(email: email, token: token)
+                authenticationToken.send(token)
             } catch let error {
                 authenticationToken.send(completion: .failure(error))
             }
