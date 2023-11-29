56169a9c100c7dadb81c8d4df5bd4db8322a21d8
jsangyun <jsangyun1039@naver.com>
2023-11-29 15:27:48 +0900

    [iOS] Refactor : SceneDelegate AppTabBarController 사용

--- iOS/Village/Village/Application/SceneDelegate.swift
+++ iOS/Village/Village/Application/SceneDelegate.swift
@@ -16,32 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        let tabBarController = UITabBarController()
-        let homeViewController = UINavigationController(rootViewController: HomeViewController())
-        let chatListViewController = UINavigationController(rootViewController: ChatListViewController())
-        let myPageViewController = UINavigationController(rootViewController: MyPageViewController())
-        
-        tabBarController.setViewControllers(
-            [homeViewController, chatListViewController, myPageViewController],
-            animated: true
-        )
-        
-        tabBarController.tabBar.tintColor = .primary500
-        tabBarController.tabBar.unselectedItemTintColor = .primary500
-        
-        if let items = tabBarController.tabBar.items {
-            items[0].selectedImage = UIImage(systemName: ImageSystemName.houseFill.rawValue)
-            items[0].image = UIImage(systemName: ImageSystemName.house.rawValue)
-            items[0].title = "홈"
-            
-            items[1].selectedImage = UIImage(systemName: ImageSystemName.messageFill.rawValue)
-            items[1].image = UIImage(systemName: ImageSystemName.message.rawValue)
-            items[1].title = "채팅"
-            
-            items[2].selectedImage = UIImage(systemName: ImageSystemName.personFill.rawValue)
-            items[2].image = UIImage(systemName: ImageSystemName.person.rawValue)
-            items[2].title = "내 정보"
-        }
+        let tabBarController = AppTabBarController()
         
         window?.rootViewController = tabBarController
         window?.makeKeyAndVisible()
