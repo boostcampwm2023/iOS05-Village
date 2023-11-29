d3434367a52d5c4962241940b5d2b87e11a0191e
koomin1227 <koomin1227@naver.com>
2023-11-29 11:40:26 +0900

    [BE] Feat : 빈 방인 경우 방 삭제

--- BE/src/chat/chats.gateway.ts
+++ BE/src/chat/chats.gateway.ts
@@ -36,21 +36,26 @@ export class ChatsGateway implements OnGatewayConnection {
 
   @SubscribeMessage('join-room')
   joinRoom(
-    @MessageBody() roomName: string,
+    @MessageBody() message: object,
     @ConnectedSocket() client: Websocket,
   ) {
     // MessageBody 에 있는 (a , b) 쌍을 통해 DB 에서 해당 방을 찾아서 roomName 을 설정해야함
-
+    const roomName = message['room'];
     if (this.rooms.has(roomName)) this.rooms.get(roomName).add(client);
     else this.rooms.set(roomName, new Set([client]));
   }
 
   @SubscribeMessage('leave-room')
   leaveRoom(
-    @MessageBody() roomName: string,
+    @MessageBody() message: object,
     @ConnectedSocket() client: Websocket,
   ) {
-    this.rooms.get(roomName).delete(client);
+    const roomName = message['room'];
+    const room = this.rooms.get(roomName);
+    room.delete(client);
+    if (room.size === 0) {
+      this.rooms.delete(roomName);
+    }
     console.log(this.rooms);
   }
 }
