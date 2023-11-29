1cda361a7f419f85d5ca1321f1d3c650e9437efd
jsangyun <jsangyun1039@naver.com>
2023-11-29 17:47:36 +0900

    [iOS] Feat : LoginViewModel 구현

---
+++ iOS/Village/Village/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -0,0 +1,71 @@
+//
+//  LoginViewModel.swift
+//  Village
+//
+//  Created by 정상윤 on 11/27/23.
+//
+
+import Foundation
+import Combine
+
+final class LoginViewModel {
+    
+    private var cancellableBag = Set<AnyCancellable>()
+    private var authenticationToken = PassthroughSubject<AuthenticationToken, Error>()
+    
+    func transform(input: Input) -> Output {
+        Publishers.Zip(input.identityToken, input.authorizationCode)
+            .sink(receiveValue: { [weak self] (token, code) in
+                guard let tokenString = String(data: token, encoding: .utf8),
+                      let codeString = String(data: code, encoding: .utf8) else { return }
+                
+                self?.handleLoginRequest(dto: AppleOAuthDTO(identityToken: tokenString, authorizationCode: codeString))
+            })
+            .store(in: &cancellableBag)
+        
+        return Output(authenticationToken: authenticationToken.eraseToAnyPublisher())
+    }
+    
+    private func handleLoginRequest(dto: AppleOAuthDTO) {
+        // TODO: 키체인에 저장된 토큰이 있는지 확인 후 서버 요청하도록 구현
+        let endpoint = APIEndPoints.loginAppleOAuth(with: dto)
+        Task {
+            do {
+                guard let responseToken = try await APIProvider.shared.request(with: endpoint) else { return }
+                authenticationToken.send(responseToken)
+            } catch let error {
+                authenticationToken.send(completion: .failure(error))
+            }
+        }
+    }
+    
+}
+
+extension LoginViewModel {
+    
+    struct Input {
+        var identityToken: AnyPublisher<Data, Never>
+        var authorizationCode: AnyPublisher<Data, Never>
+    }
+    
+    struct Output {
+        var authenticationToken: AnyPublisher<AuthenticationToken, Error>
+    }
+    
+}
+
+enum AuthenticationError: LocalizedError {
+    
+    case invalidToken
+    case serverError
+    
+    var errorDescription: String {
+        switch self {
+        case .invalidToken:
+            "토큰이 유효하지 않습니다."
+        case .serverError:
+            "서버 에러가 발생했습니다."
+        }
+    }
+    
+}
