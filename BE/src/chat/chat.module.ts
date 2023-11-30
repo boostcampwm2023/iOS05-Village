d1387923c4189a0f8560da0c5c78fab205425a99
koomin1227 <koomin1227@naver.com>
2023-11-30 18:07:54 +0900

    [BE] Feat : test push 추가

--- BE/src/chat/chat.module.ts
+++ BE/src/chat/chat.module.ts
@@ -5,10 +5,18 @@ import { ChatsGateway } from './chats.gateway';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ChatRoomEntity } from '../entities/chatRoom.entity';
 import { PostEntity } from '../entities/post.entity';
+import { FcmHandler } from '../utils/fcmHandler';
+import { RegistrationTokenEntity } from '../entities/registrationToken.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ChatRoomEntity, PostEntity])],
+  imports: [
+    TypeOrmModule.forFeature([
+      ChatRoomEntity,
+      PostEntity,
+      RegistrationTokenEntity,
+    ]),
+  ],
   controllers: [ChatController],
-  providers: [ChatService, ChatsGateway],
+  providers: [ChatService, ChatsGateway, FcmHandler],
 })
 export class ChatModule {}
