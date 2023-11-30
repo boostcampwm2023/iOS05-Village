6188c6bd03405cdbcbbf780b7e458b1c7029b251
koomin1227 <koomin1227@naver.com>
2023-11-30 16:34:34 +0900

    [BE] Feat : FCM 을 조작할 수 있는 class 구현

---
+++ BE/src/utils/fcmHandler.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import admin from 'firebase-admin';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RegistrationTokenEntity } from '../entities/registrationToken.entity';
+
+interface PushMessage {
+  title: string;
+  body: string;
+  data: any;
+}
+@Injectable()
+export class FcmHandler {
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(RegistrationTokenEntity)
+    private registrationTokenRepository: Repository<RegistrationTokenEntity>,
+  ) {
+    admin.initializeApp({
+      credential: admin.credential.cert(
+        this.configService.get('GOOGLE_APPLICATION_CREDENTIALS'),
+      ),
+    });
+  }
+
+  async sendPush(userId: string, pushMessage: PushMessage) {
+    const registrationToken = await this.getRegistrationToken(userId);
+    const message = {
+      token: registrationToken,
+      notification: {
+        title: pushMessage.title,
+        body: pushMessage.body,
+      },
+      data: {
+        room_id: pushMessage.data.room_id.toString(),
+      },
+    };
+    admin
+      .messaging()
+      .send(message)
+      .then((response) => {
+        // Response is a message ID string.
+        console.log('Successfully sent message:', response);
+      })
+      .catch((error) => {
+        console.log('Error sending message:', error);
+      });
+  }
+
+  private async getRegistrationToken(userId: string): Promise<string> {
+    const registrationToken = await this.registrationTokenRepository.findOne({
+      where: { user_hash: userId },
+    });
+    if (registrationToken === null) {
+      throw new Error('no registration token');
+    }
+    return registrationToken.registration_token;
+  }
+}
