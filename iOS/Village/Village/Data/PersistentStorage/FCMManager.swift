a09d173d3ea43401888bb4f622dd79f4a9f48c8a
SungMinCho-Kor <rhxoeh0011@gmail.com>
2023-12-03 23:37:04 +0900

    [iOS] Feat : FCMToken관리 객체 생성

---
+++ iOS/Village/Village/Data/PersistentStorage/FCMManager.swift
@@ -0,0 +1,40 @@
+//
+//  FCMManager.swift
+//  Village
+//
+//  Created by 조성민 on 12/3/23.
+//
+
+import Foundation
+
+final class FCMManager {
+    
+    static let shared = FCMManager()
+    
+    var fcmToken: String?
+    
+    init() {
+        setNotification()
+    }
+    
+    private func setNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(fcmTokenCalled), name: .fcmToken, object: nil)
+    }
+    
+    @objc private func fcmTokenCalled() {
+        sendFCMToken()
+    }
+    
+    func sendFCMToken() {
+        if JWTManager.shared.get() != nil {
+            guard let token = fcmToken else { return }
+            let endPoint = APIEndPoints.fcmTokenSend(fcmToken: token)
+            Task {
+                do {
+                    let _ = try await APIProvider.shared.request(with: endPoint)
+                }
+            }
+        }
+    }
+    
+}
