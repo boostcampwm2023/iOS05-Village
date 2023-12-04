bcf50f1c0e9b3da1fb85ccf63361150317287cc7
jsangyun <jsangyun1039@naver.com>
2023-12-05 00:15:49 +0900

    [iOS] Refactor : APIEndPoints header 삭제

--- iOS/Village/Village/Data/Network/APIEndPoints.swift
+++ iOS/Village/Village/Data/Network/APIEndPoints.swift
@@ -11,19 +11,12 @@ struct APIEndPoints {
     
     static let baseURL = "https://www.village-api.shop/"
     
-    static var header: [String: String]? {
-        guard let accessToken = JWTManager.shared.get()?.accessToken else { return nil }
-        
-        return ["Authorization": "Bearer \(accessToken)"]
-    }
-    
     static func getPosts(with requestDTO: PostListRequestDTO) -> EndPoint<[PostListResponseDTO]> {
         return EndPoint(
             baseURL: baseURL,
             path: "posts",
             method: .GET,
-            queryParameters: requestDTO,
-            headers: header
+            queryParameters: requestDTO
         )
     }
     
@@ -31,8 +24,7 @@ struct APIEndPoints {
         return EndPoint(
             baseURL: baseURL,
             path: "posts/\(id)",
-            method: .GET,
-            headers: header
+            method: .GET
         )
     }
     
@@ -40,8 +32,7 @@ struct APIEndPoints {
         return EndPoint(
             baseURL: baseURL,
             path: "users/\(id)",
-            method: .GET,
-            headers: header
+            method: .GET
         )
     }
     
@@ -51,7 +42,7 @@ struct APIEndPoints {
             path: "posts",
             method: .POST,
             bodyParameters: requestDTO.httpBody,
-            headers: header?.mergeWith(["Content-Type": "multipart/form-data; boundary=\(requestDTO.boundary)"])
+            headers: ["Content-Type": "multipart/form-data; boundary=\(requestDTO.boundary)"]
         )
     }
     
@@ -60,8 +51,7 @@ struct APIEndPoints {
             baseURL: baseURL,
             path: "chat",
             method: .GET,
-            queryParameters: chatListResponse,
-            headers: header
+            queryParameters: chatListResponse
         )
     }
     
@@ -79,8 +69,7 @@ struct APIEndPoints {
         EndPoint(
             baseURL: baseURL,
             path: "login/expire",
-            method: .GET,
-            headers: header
+            method: .GET
         )
     }
     
@@ -92,14 +81,8 @@ struct APIEndPoints {
             path: "login/refresh",
             method: .POST,
             bodyParameters: body,
-            headers: header?.mergeWith(["Content-Type": "application/json"])
+            headers: ["Content-Type": "application/json"]
         )
     }
   
-}
-
-fileprivate extension Dictionary<String, String> {
-    func mergeWith(_ dict: [String: String]) -> [String: String]? {
-        return self.merging(dict) { (current, _) in current }
-    }
 }
