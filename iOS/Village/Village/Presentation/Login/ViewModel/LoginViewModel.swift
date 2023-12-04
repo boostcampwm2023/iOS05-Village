e5fb9ea67fad20fa0c05d0812572341c6caa4999
jsangyun <jsangyun1039@naver.com>
2023-12-05 01:04:29 +0900

    [iOS] Refactor: JWT 파싱 코드 JWTManager로 이동

--- iOS/Village/Village/Presentation/Login/ViewModel/LoginViewModel.swift
+++ iOS/Village/Village/Presentation/Login/ViewModel/LoginViewModel.swift
@@ -56,34 +56,3 @@ extension LoginViewModel {
     }
     
 }
-
-fileprivate extension String {
-    
-    func base64UrlDecode(_ value: String) -> Data? {
-        var base64 = value
-            .replacingOccurrences(of: "-", with: "+")
-            .replacingOccurrences(of: "_", with: "/")
-
-        let length = Double(base64.lengthOfBytes(using: String.Encoding.utf8))
-        let requiredLength = 4 * ceil(length / 4.0)
-        let paddingLength = requiredLength - length
-        if paddingLength > 0 {
-            let padding = "".padding(toLength: Int(paddingLength), withPad: "=", startingAt: 0)
-            base64 += padding
-        }
-        return Data(base64Encoded: base64, options: .ignoreUnknownCharacters)
-    }
-    
-    func decodeJWTPart(_ value: String) -> [String: Any]? {
-        guard let bodyData = base64UrlDecode(value),
-              let json = try? JSONSerialization.jsonObject(with: bodyData, options: []),
-              let payload = json as? [String: Any] else { return nil }
-        return payload
-    }
-    
-    func decode() -> [String: Any] {
-        let segments = self.components(separatedBy: ".")
-        return decodeJWTPart(segments[1]) ?? [:]
-    }
-    
-}
