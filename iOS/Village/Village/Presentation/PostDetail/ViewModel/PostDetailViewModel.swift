b8111df4b6d564003cc5b9a6d2dde23daa3a51db
동재 <59719500+jea0716@users.noreply.github.com>
2023-12-05 13:02:34 +0900

    [iOS] Feat : ViewModel에 방번호 알려주는 함수 구현
    - ViewModel에 방번호 알려주는 함수 구현

--- iOS/Village/Village/Presentation/PostDetail/ViewModel/PostDetailViewModel.swift
+++ iOS/Village/Village/Presentation/PostDetail/ViewModel/PostDetailViewModel.swift
@@ -12,7 +12,24 @@ final class PostDetailViewModel {
     
     private var post = PassthroughSubject<PostResponseDTO, NetworkError>()
     private var user = PassthroughSubject<UserResponseDTO, NetworkError>()
+    private var responseData: PostRoomResponseDTO?
     private var cancellableBag = Set<AnyCancellable>()
+    
+    func createChatRoom(writer: String, postID: Int) -> PostRoomResponseDTO? {
+        let request = PostRoomRequestDTO(writer: writer, postID: postID)
+        let endpoint = APIEndPoints.postCreateChatRoom(with: request)
+        Task {
+            do {
+                guard let data = try await APIProvider.shared.request(with: endpoint) else { return }
+                print(data)
+                responseData = data
+            } catch let error as NetworkError {
+                dump(error)
+            }
+        }
+        
+        return responseData
+    }
     
     func transform(input: Input) -> Output {
         input.postID
