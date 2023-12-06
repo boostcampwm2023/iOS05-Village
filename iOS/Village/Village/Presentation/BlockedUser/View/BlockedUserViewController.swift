e21984272caf15c178db7ccda2049f5ee120eed3
동재 <59719500+jea0716@users.noreply.github.com>
2023-12-06 16:35:33 +0900

    [iOS] Feat : 네비게이션 바 UI 설정
    네비게이션 바 UI 설정

--- iOS/Village/Village/Presentation/BlockedUser/View/BlockedUserViewController.swift
+++ iOS/Village/Village/Presentation/BlockedUser/View/BlockedUserViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class BlockedUserViewController: UIViewController {
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setUI()
     }
 
+}
+
+extension BlockedUserViewController {
+    
+    private func setUI() {
+        setNavigationUI()
+    }
+    
+    private func setNavigationUI() {
+        self.navigationItem.title = "차단 관리"
+    }
+    
 }
