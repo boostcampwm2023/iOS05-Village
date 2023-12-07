a3e36766fb621d6080375a788e8549e40426a278
동재 <59719500+jea0716@users.noreply.github.com>
2023-12-07 14:07:42 +0900

    [iOS] Chore : 채팅관련 DTO 수정
    - 채팅관련 DTO 수정

--- iOS/Village/Village/Domain/Network/GetRoomResponseDTO.swift
+++ iOS/Village/Village/Domain/Network/GetRoomResponseDTO.swift
@@ -12,17 +12,21 @@ struct Chat: Hashable, Codable {
     let id: Int
     let message: String
     let sender: String
+    let chatRoom: Int
     let isRead: Bool
     let createDate: String
     let deleteDate: String?
+    let count: Int
     
     enum CodingKeys: String, CodingKey {
         case id
         case message
         case sender
+        case chatRoom = "chat_room"
         case isRead = "is_read"
         case createDate = "create_date"
         case deleteDate = "delete_date"
+        case count
     }
     
 }
