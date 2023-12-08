55e6d8fdae32693cb2dda9a27b157282a0387706
SungMinCho-Kor <rhxoeh0011@gmail.com>
2023-12-08 22:44:50 +0900

    [iOS] Feat : UIImage downsampling으로 용량 줄이기 구현

--- iOS/Village/Village/Presentation/Utils/Extensions/UIImage+Resize.swift
+++ iOS/Village/Village/Presentation/Utils/Extensions/UIImage+Resize.swift
@@ -5,4 +5,31 @@
 //  Created by 조성민 on 12/8/23.
 //
 
-import Foundation
+import UIKit
+
+extension UIImage {
+    
+    func resize(newWidth: CGFloat, newHeight: CGFloat) -> UIImage {
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        
+        return renderImage
+    }
+    
+    func resizeKeepScale(newWidth: CGFloat) -> UIImage {
+        let scale = newWidth / self.size.width
+        let newHeight = self.size.height * scale
+        
+        let size = CGSize(width: newWidth, height: newHeight)
+        let render = UIGraphicsImageRenderer(size: size)
+        let renderImage = render.image { _ in
+            self.draw(in: CGRect(origin: .zero, size: size))
+        }
+        
+        return renderImage
+    }
+    
+}
