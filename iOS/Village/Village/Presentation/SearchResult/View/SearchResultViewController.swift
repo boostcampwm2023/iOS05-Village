51c637cad04ad82e744b11a4d42a0de17babab8b
동재 <59719500+jea0716@users.noreply.github.com>
2023-12-09 20:17:25 +0900

    [iOS] Feat : segmentControl 생성
    - segmentControl 생성

--- iOS/Village/Village/Presentation/SearchResult/View/SearchResultViewController.swift
+++ iOS/Village/Village/Presentation/SearchResult/View/SearchResultViewController.swift
@@ -13,6 +13,7 @@ final class SearchResultViewController: UIViewController {
     typealias ViewModel = SearchResultViewModel
     
     private let viewModel = ViewModel()
+    private let togglePublisher = PassthroughSubject<Void, Never>()
     private var cancellableBag = Set<AnyCancellable>()
     
     private let postTitle: String
@@ -22,6 +23,16 @@ final class SearchResultViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
     
+    private lazy var requestSegmentedControl: UISegmentedControl = {
+        let control = UISegmentedControl(items: ["대여", "요청"])
+        control.translatesAutoresizingMaskIntoConstraints = false
+        control.addTarget(self, action: #selector(segmentedControlChanged), for: .valueChanged)
+        control.selectedSegmentIndex = 0
+        control.selectedSegmentTintColor = .primary500
+        
+        return control
+    }()
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
@@ -29,13 +40,29 @@ final class SearchResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .primary500
+        setUI()
         bindViewModel()
     }
 
 }
 
 extension SearchResultViewController {
+    
+    private func setUI() {
+        self.view.backgroundColor = .systemBackground
+        self.view.addSubview(requestSegmentedControl)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            requestSegmentedControl.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 10),
+            requestSegmentedControl.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -10),
+            requestSegmentedControl.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 10),
+            requestSegmentedControl.heightAnchor.constraint(equalToConstant: 35)
+        ])
+    }
     
     private func bindViewModel() {
         let input = ViewModel.Input(postTitle: Just(postTitle).eraseToAnyPublisher())
@@ -53,6 +80,10 @@ extension SearchResultViewController {
                 // TODO: 데이터 테이블 뷰? 컬렉션 뷰에 추가하기
             }
             .store(in: &cancellableBag)
+    }
+    
+    @objc private func segmentedControlChanged() {
+        togglePublisher.send()
     }
     
 }
