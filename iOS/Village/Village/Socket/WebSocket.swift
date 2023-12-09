249f7b235e2899a6450dd550a2f05acd76f39283
동재 <59719500+jea0716@users.noreply.github.com>
2023-12-09 14:41:56 +0900

    [iOS] Fix : ReceiveMessageDTO 수정
    - ReceiveMessageDTO 수정

--- iOS/Village/Village/Socket/WebSocket.swift
+++ iOS/Village/Village/Socket/WebSocket.swift
@@ -10,9 +10,21 @@ import Combine
 
 class MessageManager {
     static let shared = MessageManager()
-    let messageSubject = PassthroughSubject<ReceiveMessage, Never>()
+    let messageSubject = PassthroughSubject<ReceiveMessageDTO, Never>()
     
     private init() {}
+}
+
+struct ReceiveMessageDTO: Hashable, Codable {
+
+    let event: String
+    let data: ReceiveMessage
+
+    enum CodingKeys: String, CodingKey {
+        case event
+        case data
+    }
+
 }
 
 struct ReceiveMessage: Hashable, Codable {
@@ -109,7 +121,7 @@ final class WebSocket: NSObject {
                     if let jsonData = text.data(using: .utf8) {
                         do {
                             let decoder = JSONDecoder()
-                            let message = try decoder.decode(ReceiveMessage.self, from: jsonData)
+                            let message = try decoder.decode(ReceiveMessageDTO.self, from: jsonData)
                             MessageManager.shared.messageSubject.send(message)
                         } catch {
                             dump(error)
