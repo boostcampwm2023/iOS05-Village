64ba1c7729fc080380166436f474a2e9a9f1df75
동재 <59719500+jea0716@users.noreply.github.com>
2023-12-09 19:10:54 +0900

    [iOS] Feat : SearchResultViewController 생성
    - SearchResultViewController 생성

---
+++ iOS/Village/Village/Presentation/SearchResult/SearchResultViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultViewController.swift
+//  Village
+//
+//  Created by 박동재 on 12/9/23.
+//
+
+import UIKit
+
+class SearchResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .primary500
+    }
+
+}
