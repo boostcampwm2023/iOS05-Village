4ae14411399f70a6f16181c4e34f84f217752299
koomin1227 <koomin1227@naver.com>
2023-12-11 00:56:45 +0900

    [BE] Feat : report 등록 controller 구현

--- BE/src/report/report.controller.ts
+++ BE/src/report/report.controller.ts
@@ -1,12 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './createReport.dto';
 import { ReportService } from './report.service';
+import { AuthGuard } from '../utils/auth.guard';
+import { UserHash } from '../utils/auth.decorator';
 
 @Controller('report')
+@UseGuards(AuthGuard)
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
   @Post()
-  async createReport(@Body() body: CreateReportDto) {
-    await this.reportService.createReport(body);
+  async createReport(
+    @Body() body: CreateReportDto,
+    @UserHash() userId: string,
+  ) {
+    await this.reportService.createReport(body, userId);
   }
 }
