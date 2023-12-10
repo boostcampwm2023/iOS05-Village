5de5fb2efa73bb9a4db363ca212a606c1309c129
SungMinCho-Kor <rhxoeh0011@gmail.com>
2023-12-11 01:14:46 +0900

    [iOS] Feat : 신고 내용 글자수 제한

--- iOS/Village/Village/Presentation/Report/View/ReportViewController.swift
+++ iOS/Village/Village/Presentation/Report/View/ReportViewController.swift
@@ -38,14 +38,24 @@ final class ReportViewController: UIViewController {
             right: 10
         )
         textView.font = .systemFont(ofSize: 14)
+        textView.delegate = self
         
         return textView
+    }()
+    
+    private lazy var descriptionCountLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "글자수 제한: 0/300"
+        label.font = .systemFont(ofSize: 12)
+        
+        return label
     }()
     
     private lazy var infoLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.text = "신고 내용이 사실과 다를 경우 이용 제재를 받을 수 있으니 주의해주세요."
+        label.text = "신고 내용이 사실과 다를 경우 이용 제재를 받을 수 있습니다."
         label.font = .systemFont(ofSize: 14)
         label.textColor = .darkGray
         
@@ -68,7 +78,7 @@ final class ReportViewController: UIViewController {
     
     private lazy var completeAlert: UIAlertController = {
         let alert = UIAlertController(title: "접수 완료", message: "신고가 접수되었습니다.", preferredStyle: .alert)
-        let action = UIAlertAction(title: "신고가 접수되었습니다.", style: .default) { [weak self] action in
+        let action = UIAlertAction(title: "완료", style: .default) { [weak self] _ in
             self?.navigationController?.popViewController(animated: true)
         }
         alert.addAction(action)
@@ -107,6 +117,7 @@ private extension ReportViewController {
         
         view.addSubview(headerLabel)
         view.addSubview(contentTextView)
+        view.addSubview(descriptionCountLabel)
         view.addSubview(infoLabel)
         view.addSubview(reportButton)
     }
@@ -125,15 +136,21 @@ private extension ReportViewController {
         ])
         
         NSLayoutConstraint.activate([
-            infoLabel.topAnchor.constraint(equalTo: contentTextView.bottomAnchor, constant: 10),
-            infoLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10)
+            descriptionCountLabel.topAnchor.constraint(equalTo: contentTextView.bottomAnchor, constant: 10),
+            descriptionCountLabel.trailingAnchor.constraint(equalTo: contentTextView.trailingAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            infoLabel.topAnchor.constraint(equalTo: descriptionCountLabel.bottomAnchor, constant: 10),
+            infoLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
         
         NSLayoutConstraint.activate([
             reportButton.topAnchor.constraint(equalTo: infoLabel.bottomAnchor, constant: 10),
             reportButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 10),
             reportButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            reportButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+            reportButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            reportButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
     
@@ -154,6 +171,23 @@ private extension ReportViewController {
                 self?.present(alert, animated: true)
             }
             .store(in: &cancellableBag)
+    }
+    
+}
+
+extension ReportViewController: UITextViewDelegate {
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        let currentText = textView.text ?? ""
+        guard let stringRange = Range(range, in: currentText) else { return false }
+        let changedText = currentText.replacingCharacters(in: stringRange, with: text)
+        if changedText.count <= 300 {
+            descriptionCountLabel.text = "글자수 제한 : \(changedText.count)/300"
+            descriptionCountLabel.textColor = .label
+        } else {
+            descriptionCountLabel.textColor = .negative400
+        }
+        return changedText.count <= 300
     }
     
 }
