d461966184643186f0e8e034a86736b0ca6baf30
jsangyun <jsangyun1039@naver.com>
2023-12-11 19:38:25 +0900

    [iOS] Rename: Int+, NotificationName+ Common으로 이동

---
+++ iOS/Village/Village/Common/NotificationName+.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationName+.swift
+//  Village
+//
+//  Created by 조성민 on 11/29/23.
+//
+
+import Foundation
+
+extension Notification.Name {
+    
+    static let openChatRoom = Notification.Name("OpenChatRoom")
+    static let fcmToken = Notification.Name("FCMToken")
+    static let loginSucceed = Notification.Name("LoginSucceed")
+    static let shouldLogin = Notification.Name("ShouldLogin")
+    static let postEdited = Notification.Name("PostEdited")
+    static let postDeleted = Notification.Name("PostDeleted")
+    static let postCreated = Notification.Name("PostCreated")
+  
+}
