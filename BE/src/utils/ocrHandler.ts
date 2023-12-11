43dc79ad0d4c5c3faf6ed8f848ec0c763c987ede
koomin <koomin1227@naver.com>
2023-12-11 16:26:44 +0900

    [BE] Refactor : 주석제거

--- BE/src/utils/ocrHandler.ts
+++ BE/src/utils/ocrHandler.ts
@@ -7,7 +7,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class OcrHandler {
   constructor(private configService: ConfigService) {}
-  // configService = new ConfigService();
   async convertImageToText(file: Express.Multer.File): Promise<string> {
     const res = await this.sendOcrRequest(file);
     let text;
@@ -19,7 +18,6 @@ export class OcrHandler {
     }
   }
   parseTextFromOcr(fields: Array<object>): string {
-    //util
     let text: string = '';
     for (let i = 0; i < fields.length; i++) {
       text += fields[i]['inferText'] + ' ';
@@ -29,7 +27,6 @@ export class OcrHandler {
   }
 
   async sendOcrRequest(file: Express.Multer.File) {
-    //util
     const imgInfo = path.parse(file.originalname);
     const imgBuffer = file.buffer;
     return await axios.post(
@@ -54,50 +51,3 @@ export class OcrHandler {
     );
   }
 }
-
-// const configService = new ConfigService();
-// export async function convertImageToText(
-//   file: Express.Multer.File,
-// ): Promise<string> {
-//   const res = await sendOcrRequest(file);
-//   let text;
-//   if (res.status === 200) {
-//     text = parseTextFromOcr(res.data.images[0].fields);
-//   } // 에러 처리 해야댐
-//   return text;
-// }
-// function parseTextFromOcr(fields: Array<object>): string {
-//   //util
-//   let text: string = '';
-//   for (let i = 0; i < fields.length; i++) {
-//     text += fields[i]['inferText'] + ' ';
-//     if (fields[i]['lineBreak'] === true) text += '\n';
-//   }
-//   return text;
-// }
-//
-// async function sendOcrRequest(file: Express.Multer.File) {
-//   //util
-//   const imgInfo = path.parse(file.originalname);
-//   const imgBuffer = file.buffer;
-//   return await axios.post(
-//     configService.get('CLOVA_URL'), // APIGW Invoke URL
-//     {
-//       images: [
-//         {
-//           format: imgInfo.ext.slice(1), // file format
-//           name: imgInfo.name, // image name
-//           data: imgBuffer.toString('base64'), // image base64 string(only need part of data). Example: base64String.split(',')[1]
-//         },
-//       ],
-//       requestId: uuid(), // unique string
-//       timestamp: 0,
-//       version: 'V2',
-//     },
-//     {
-//       headers: {
-//         'X-OCR-SECRET': configService.get('CLOVA_SECRET'), // Secret Key
-//       },
-//     },
-//   );
-// }
