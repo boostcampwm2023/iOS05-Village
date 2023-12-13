b962cc53b633c672eb0ca6b1a39ba625e86840cc
koomin1227 <koomin1227@naver.com>
2023-12-13 16:00:01 +0900

    [BE] Feat : 탈퇴한 회원을 차단후 조회했을 때 null 반환하게 수정

--- BE/src/users-block/users-block.service.ts
+++ BE/src/users-block/users-block.service.ts
@@ -5,6 +5,12 @@ import { Repository } from 'typeorm';
 import { UserEntity } from 'src/entities/user.entity';
 import { ConfigService } from '@nestjs/config';
 
+interface BlockedUser {
+  user_id: string;
+  nickname?: string;
+  profile_img?: string;
+}
+
 @Injectable()
 export class UsersBlockService {
   constructor(
@@ -52,15 +58,19 @@ export class UsersBlockService {
     });
 
     const blockedUsers = res.reduce((acc, cur) => {
-      const user = {
-        nickname: cur.blockedUser.nickname,
-        profile_img:
+      const user: BlockedUser = {
+        user_id: cur.blocked_user,
+      };
+      if (cur.blockedUser === null) {
+        user.nickname = null;
+        user.profile_img = null;
+      } else {
+        user.nickname = cur.blockedUser.nickname;
+        user.profile_img =
           cur.blockedUser.profile_img === null
             ? this.configService.get('DEFAULT_PROFILE_IMAGE')
-            : cur.blockedUser.profile_img,
-        user_id: cur.blockedUser.user_hash,
-      };
-
+            : cur.blockedUser.profile_img;
+      }
       acc.push(user);
       return acc;
     }, []);
