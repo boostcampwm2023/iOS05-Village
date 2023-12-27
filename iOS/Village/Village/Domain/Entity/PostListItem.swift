1cd4bc4fd5bd59e3adb1f69f16ee5ffd9f515697
jsangyun <jsangyun1039@naver.com>
2023-12-27 15:14:27 +0900

    [iOS] Feat: PostListItem 엔티티 구현

---
+++ iOS/Village/Village/Domain/Entity/PostListItem.swift
@@ -0,0 +1,33 @@
+//
+//  PostListItem.swift
+//  Village
+//
+//  Created by 정상윤 on 12/27/23.
+//
+
+import Foundation
+
+struct PostListItem {
+    
+    let title: String
+    let price: Int?
+    let postID: Int
+    let userID: String
+    let thumbnailURL: String
+    let isRequest: Bool
+    let startDate: String
+    let endDate: String
+    
+}
+
+extension PostListItem: Hashable {
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(postID)
+    }
+    
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.postID == rhs.postID
+    }
+     
+}
