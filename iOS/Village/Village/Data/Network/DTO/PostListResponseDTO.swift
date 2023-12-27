cee8cbbee3308b8bf17d23e894ab6e030ada2efc
jsangyun <jsangyun1039@naver.com>
2023-12-27 15:09:46 +0900

    [iOS] Feat: PostListResponseDTO 구현

---
+++ iOS/Village/Village/Data/Network/DTO/PostListResponseDTO.swift
@@ -0,0 +1,32 @@
+//
+//  PostListResponseDTO.swift
+//  Village
+//
+//  Created by 정상윤 on 12/27/23.
+//
+
+import Foundation
+
+struct PostListResponseDTO: Decodable {
+    
+    let title: String
+    let price: Int?
+    let postID: Int
+    let userID: String
+    let postImage: String
+    let isRequest: Bool
+    let startDate: String
+    let endDate: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case price
+        case postID = "post_id"
+        case userID = "user_id"
+        case isRequest = "is_request"
+        case postImage = "post_image"
+        case startDate = "start_date"
+        case endDate = "end_date"
+    }
+    
+}
