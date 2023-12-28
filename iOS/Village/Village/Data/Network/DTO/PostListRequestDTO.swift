6a8ea20400e594259b43ab75b46ffc5800d2cccf
jsangyun <jsangyun1039@naver.com>
2023-12-28 11:24:23 +0900

    [iOS] Refactor: PostListRequestDTO 서버 스펙에 맞게 변경

--- iOS/Village/Village/Data/Network/DTO/PostListRequestDTO.swift
+++ iOS/Village/Village/Data/Network/DTO/PostListRequestDTO.swift
@@ -12,20 +12,25 @@ struct PostListRequestDTO: Encodable {
     let searchKeyword: String?
     let requestFilter: String?
     let writer: String?
-    let page: String?
+    let cursorID: String?
     
-    init(searchKeyword: String? = nil, requestFilter: String? = nil, writer: String? = nil, page: String? = nil) {
+    init(
+        searchKeyword: String? = nil,
+        requestFilter: String? = nil,
+        writer: String? = nil,
+        lastID: String? = nil
+    ) {
         self.searchKeyword = searchKeyword
         self.requestFilter = requestFilter
         self.writer = writer
-        self.page = page
+        self.cursorID = lastID
     }
     
     enum CodingKeys: String, CodingKey {
         case searchKeyword
         case requestFilter
         case writer
-        case page
+        case cursorID = "cursorId"
     }
     
 }
