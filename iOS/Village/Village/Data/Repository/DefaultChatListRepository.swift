211867dcf8b352350cdf1772fb0a1156ecef5058
동재 <59719500+jea0716@users.noreply.github.com>
2024-01-06 19:02:50 +0900

    [iOS] Feat : DefaultChatListRepository 생성
    - DefaultChatListRepository 생성

---
+++ iOS/Village/Village/Data/Repository/DefaultChatListRepository.swift
@@ -0,0 +1,33 @@
+//
+//  DefaultChatListRepository.swift
+//  Village
+//
+//  Created by 박동재 on 1/6/24.
+//
+
+import Foundation
+
+struct DefaultChatListRepository: ChatListRepository {
+    
+    private func makeEndPoint() -> EndPoint<GetChatListResponseDTO> {
+        return EndPoint(
+            baseURL: Constant.baseURL,
+            path: "chat/room",
+            method: .GET
+        )
+    }
+    
+    func fetchChatList() async -> Result<GetChatListResponseDTO, Error> {
+        let endpoint = makeEndPoint()
+        
+        do {
+            guard let chatListDTO = try await APIProvider.shared.request(with: endpoint) else {
+                return .success(GetChatListResponseDTO(allRead: true, chatList: []))
+            }
+            return .success(chatListDTO)
+        } catch {
+            return .failure(error)
+        }
+    }
+    
+}
