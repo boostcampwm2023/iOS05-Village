b4039aab666a2b3f1b76102252262a749a3d843c
koomin1227 <koomin1227@naver.com>
2024-01-08 00:29:55 +0900

    [BE] Feat : NotificationService 생성자 구현

--- BE/src/notification/notification.service.ts
+++ BE/src/notification/notification.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegistrationTokenEntity } from '../entities/registrationToken.entity';
+import { Repository } from 'typeorm';
+import admin from 'firebase-admin';
 
 @Injectable()
-export class NotificationService {}
+export class NotificationService {
+  private readonly logger = new Logger('ChatsGateway');
+  constructor(
+    private configService: ConfigService,
+    @InjectRepository(RegistrationTokenEntity)
+    private registrationTokenRepository: Repository<RegistrationTokenEntity>,
+  ) {
+    if (admin.apps.length === 0) {
+      admin.initializeApp({
+        credential: admin.credential.cert(
+          this.configService.get('GOOGLE_APPLICATION_CREDENTIALS'),
+        ),
+      });
+      this.logger.log('Firebase Admin initialized');
+    }
+  }
+}
