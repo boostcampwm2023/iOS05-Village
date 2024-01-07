52e93c7e618d1df3cbe84b1bc7e289602e87d856
동재 <59719500+jea0716@users.noreply.github.com>
2024-01-07 17:24:49 +0900

    [iOS] Feat : DefaultChatRoomRepository 생성
    - DefaultChatRoomRepository 생성

---
+++ iOS/Village/Village/Data/Repository/DefaultChatRoomRepository.swift
@@ -0,0 +1,39 @@
+//
+//  DefaultChatRoomRepository.swift
+//  Village
+//
+//  Created by 박동재 on 1/7/24.
+//
+
+import Foundation
+
+struct DefaultChatRoomRepository: ChatRoomRepository {
+    
+    typealias ResponseDTO = ChatRoomResponseDTO
+    
+    private func makeEndPoint(roomID: Int) -> EndPoint<ChatRoomResponseDTO  > {
+        return EndPoint(
+            baseURL: Constant.baseURL,
+            path: "chat/room/\(roomID)",
+            method: .GET
+        )
+    }
+    
+    func fetchRoomData(roomID: Int) async -> Result<ChatRoomResponseDTO, NetworkError> {
+        let endpoint = makeEndPoint(roomID: roomID)
+        
+        do {
+            guard let roomDataDTO = try await APIProvider.shared.request(with: endpoint) else {
+                return .success(
+                    ResponseDTO(writer: "", writerProfileIMG: "", user: "", userProfileIMG: "", postID: -1, chatLog: [])
+                )
+            }
+            return .success(roomDataDTO)
+        } catch let error as NetworkError {
+            return .failure(error)
+        } catch {
+            return .failure(.unknownError)
+        }
+    }
+    
+}
