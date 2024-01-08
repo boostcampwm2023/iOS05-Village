5dca4a8e4a3941bcf3158bcab48635fdc75f2136
koomin1227 <koomin1227@naver.com>
2024-01-08 23:43:09 +0900

    [BE] Refactor : user module의 사용하지 않는 provider 제거

--- BE/src/users/users.module.ts
+++ BE/src/users/users.module.ts
@@ -2,14 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { S3Handler } from 'src/common/S3Handler';
 import { UserEntity } from '../entities/user.entity';
 import { BlockUserEntity } from '../entities/blockUser.entity';
 import { BlockPostEntity } from '../entities/blockPost.entity';
 import { AuthGuard } from 'src/common/guard/auth.guard';
 import { RegistrationTokenEntity } from '../entities/registrationToken.entity';
-import { FcmHandler } from 'src/common/fcmHandler';
-import { GreenEyeHandler } from '../common/greenEyeHandler';
 import { UserRepository } from './user.repository';
 import { ImageModule } from '../image/image.module';
 import { NotificationModule } from '../notification/notification.module';
@@ -26,13 +23,6 @@ import { NotificationModule } from '../notification/notification.module';
     NotificationModule,
   ],
   controllers: [UsersController],
-  providers: [
-    UsersService,
-    S3Handler,
-    AuthGuard,
-    FcmHandler,
-    GreenEyeHandler,
-    UserRepository,
-  ],
+  providers: [UsersService, AuthGuard, UserRepository],
 })
 export class UsersModule {}
