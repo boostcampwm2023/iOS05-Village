3c85fdbd990eff44e424351e366caee9ea0bb1da
koomin1227 <koomin1227@naver.com>
2024-01-12 23:20:25 +0900

    [BE] Test : checkAuth 테스트

--- BE/src/users/users.service.spec.ts
+++ BE/src/users/users.service.spec.ts
@@ -3,6 +3,8 @@ import { UsersService } from './users.service';
 import { UserRepository } from './user.repository';
 import { ConfigService } from '@nestjs/config';
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { HttpException } from '@nestjs/common';
+import { UserEntity } from '../entities/user.entity';
 
 const mockRepository = {
   save: jest.fn(),
@@ -42,5 +44,28 @@ describe('UsersService', function () {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('checkAuth', function () {
+    it('should return 404', function () {
+      repository.getRepository().findOne.mockResolvedValue(null);
+      expect(async () => {
+        await service.checkAuth('user', 'user');
+      }).rejects.toThrowError(
+        new HttpException('유저가 존재하지 않습니다.', 404),
+      );
+    });
+
+    it('should return 403', function () {
+      repository.getRepository().findOne.mockResolvedValue(new UserEntity());
+      expect(async () => {
+        await service.checkAuth('user', 'user1');
+      }).rejects.toThrowError(new HttpException('수정 권한이 없습니다.', 403));
+    });
+
+    it('should pass', async function () {
+      repository.getRepository().findOne.mockResolvedValue(new UserEntity());
+      await service.checkAuth('user', 'user');
+    });
   });
 });
